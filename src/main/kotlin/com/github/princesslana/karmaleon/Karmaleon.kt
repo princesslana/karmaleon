09102d8f58422e6eb6a9cea16b55a2de681ef291
ianagbip1oti <ianagbip1oti@gmail.com>
2019-11-23 21:26:00 +0000

    📦 NEW: Add SmallD and connect to Discord

--- src/main/kotlin/com/github/princesslana/karmaleon/Karmaleon.kt
+++ src/main/kotlin/com/github/princesslana/karmaleon/Karmaleon.kt
@@ -1,5 +1,12 @@
 package com.github.princesslana.karmaleon
 
+import com.github.princesslana.smalld.SmallD
+import org.slf4j.impl.SimpleLogger;
+
 fun main(args: Array<String>) {
-    println("Hello World")
+    if (System.getenv("KRML_DEBUG")?.toBoolean() ?: false) {
+        System.setProperty(SimpleLogger.DEFAULT_LOG_LEVEL_KEY, "DEBUG")
+    }
+
+    SmallD.run(System.getenv("KRML_TOKEN")) { }
 }
