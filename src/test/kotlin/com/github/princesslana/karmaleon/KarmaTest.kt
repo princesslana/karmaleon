75bd10fb33657470695950b17630370f3c11e8b7
ianagbip1oti <ianagbip1oti@gmail.com>
2020-04-18 18:13:10 +0000

    👌 IMPROVE: Remove Karma types

--- src/test/kotlin/com/github/princesslana/karmaleon/KarmaTest.kt
+++ src/test/kotlin/com/github/princesslana/karmaleon/KarmaTest.kt
@@ -29,7 +29,7 @@ class KarmaTest {
             ).toKarma("^")
 
         assertNotNull(k)
-        assertEquals(Karma(KarmaType.HELPFUL, dummyUser), k)
+        assertEquals(Karma(dummyUser), k)
     }
 
     @Test
@@ -39,7 +39,7 @@ class KarmaTest {
             ).toKarma("!")
 
         assertNotNull(k)
-        assertEquals(Karma(KarmaType.HELPFUL, dummyUser), k)
+        assertEquals(Karma(dummyUser), k)
     }
 
     @Test
