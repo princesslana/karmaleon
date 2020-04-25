501f8cb0a6d4a1a9db31600fbbfff9373ad1cd7a
ianagbip1oti <ianagbip1oti@gmail.com>
2020-04-25 00:17:52 +0000

    📦 NEW: Add reliable async sending of replies

--- src/main/kotlin/com/github/princesslana/karmaleon/Karmaleon.kt
+++ src/main/kotlin/com/github/princesslana/karmaleon/Karmaleon.kt
@@ -1,7 +1,57 @@
 package com.github.princesslana.karmaleon
 
+import com.github.michaelbull.retry.*
+import com.github.michaelbull.retry.policy.*
+import com.github.michaelbull.retry.policy.limitAttempts
+import com.github.princesslana.smalld.SmallD
 import java.net.URLEncoder
+import kotlinx.coroutines.*
 import org.slf4j.impl.SimpleLogger
+
+class Karmaleon(val smalld: SmallD, val prefix: String, val repository: Repository) {
+
+    fun onMessageCreate(msg: Message) {
+        issueKarma(msg)
+
+        if (msg.content.startsWith("${prefix}karma")) {
+            displayKarma(msg)
+        }
+    }
+
+    fun issueKarma(msg: Message) {
+        val karma = msg.toKarma()
+
+        karma.forEach { repository.add(it) }
+
+        if (!karma.none()) {
+            val emoji = URLEncoder.encode("👍", "utf-8")
+            smalld.put(
+                "/channels/${msg.channelId}" +
+                "/messages/${msg.id}/" +
+                "reactions/$emoji/@me", "")
+        }
+    }
+
+    fun displayKarma(msg: Message) {
+        val mentions =
+            if (msg.mentions.none()) listOf(msg.author)
+            else msg.mentions.orEmpty()
+
+        val response = mentions.map {
+            "${it.tag} has ${repository.count(it)} karma"
+        }.joinToString(separator = "\n")
+
+        smalld.post("/channels/${msg.channelId}/messages", CreateMessage(response))
+    }
+
+    fun reliablyAsync(f: () -> Unit) {
+        GlobalScope.launch {
+            retry(limitAttempts(3) + constantDelay(delayMillis = 3000)) {
+                f()
+            }
+        }
+    }
+}
 
 fun main(args: Array<String>) {
     if (System.getenv("KRML_DEBUG")?.toBoolean() ?: false) {
@@ -13,29 +63,8 @@ fun main(args: Array<String>) {
     val repository = Repository()
 
     run(System.getenv("KRML_TOKEN")) {
-        onMessageCreate { msg ->
-            val karma = msg.toKarma()
+        val karmaleon = Karmaleon(this, prefix, repository)
 
-            karma.forEach { repository.add(it) }
-
-            if (!karma.none()) {
-                val emoji = URLEncoder.encode("👍", "utf-8")
-                put("/channels/${msg.channelId}" +
-                    "/messages/${msg.id}/" +
-                    "reactions/$emoji/@me", "")
-            }
-
-            if (msg.content.startsWith("${prefix}karma")) {
-                val mentions =
-                    if (msg.mentions.none()) listOf(msg.author)
-                    else msg.mentions.orEmpty()
-
-                val response = mentions.map {
-                    "${it.tag} has ${repository.count(it)} karma"
-                }.joinToString(separator = "\n")
-
-                post("/channels/${msg.channelId}/messages", CreateMessage(response))
-            }
-        }
+        onMessageCreate { karmaleon.onMessageCreate(it) }
     }
 }
